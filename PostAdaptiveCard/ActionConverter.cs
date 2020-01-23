85d833dfd9a2ebde59b354b0fd9f272debd4c62e
David Wengier <git@wengier.com>
2020-01-23 11:30:44 +1100

    Convert github issues and PRs to adaptive cards

---
+++ PostAdaptiveCard/ActionConverter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using MessageCardModel.Actions;
+
+namespace PostAdaptiveCard
+{
+    internal class ActionConverter : JsonConverter<IEnumerable<IAction>>
+    {
+        public override IEnumerable<IAction> Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Write(Utf8JsonWriter writer, IEnumerable<IAction> value, JsonSerializerOptions options)
+        {
+            foreach (IAction action in value)
+            {
+                switch (action.Type)
+                {
+                    case ActionType.ActionCard:
+                    {
+                        JsonSerializer.Serialize(writer, (ActionCardAction)action, options);
+                        break;
+                    }
+                    case ActionType.HttpPost:
+                    {
+                        JsonSerializer.Serialize(writer, (HttpPostAction)action, options);
+                        break;
+                    }
+                    case ActionType.OpenUri:
+                    {
+                        JsonSerializer.Serialize(writer, (OpenUriAction)action, options);
+                        break;
+                    }
+                    default:
+                    {
+                        throw new NotSupportedException($"No idea what a {action.Type} action is");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ PostAdaptiveCard/Program.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using MessageCardModel;
+using MessageCardModel.Actions;
+using MessageCardModel.Actions.OpenUri;
+
+namespace PostAdaptiveCard
+{
+    internal class Program
+    {
+        private static void Main(string webhookUri, string eventPath, string eventName)
+        {
+            using FileStream fs = File.OpenRead(eventPath);
+            var doc = JsonDocument.Parse(fs);
+
+            MessageCard card;
+            if (doc.RootElement.TryGetProperty("issue", out JsonElement issue))
+            {
+                card = GetIssueCard(eventName, issue);
+            }
+            else if (doc.RootElement.TryGetProperty("pull_request", out JsonElement pullRequest))
+            {
+                card = GetPullRequestCard(eventName, pullRequest);
+            }
+            else
+            {
+                throw new InvalidOperationException("Can not support this type of event.");
+            }
+
+            var options = new JsonSerializerOptions
+            {
+                IgnoreNullValues = true,
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                WriteIndented = true
+            };
+            options.Converters.Add(new ActionConverter());
+            string converted = JsonSerializer.Serialize(card, options);
+
+            Console.WriteLine(converted);
+
+            if (!string.IsNullOrWhiteSpace(webhookUri))
+            {
+                using var client = new HttpClient();
+                using var content = new StringContent(converted, Encoding.UTF8, "application/json");
+                using HttpResponseMessage response = client.PostAsync(webhookUri, content).Result;
+                response.EnsureSuccessStatusCode();
+            }
+
+            static MessageCard GetPullRequestCard(string eventName, JsonElement pullRequest)
+            {
+                // pullRequest is one of these: https://developer.github.com/v3/pulls/#response
+
+                JsonElement user = pullRequest.GetProperty("user");
+                return new MessageCard
+                {
+
+                    Title = $"PR #{pullRequest.GetProperty("number")} {eventName} - {pullRequest.GetProperty("title")}",
+                    Summary = $"PR #{pullRequest.GetProperty("number")} {eventName} by { user.GetProperty("login")}, {pullRequest.GetProperty("title")}",
+                    Sections = new List<Section>()
+                    {
+                        new Section
+                        {
+                            ActivityTitle = user.GetProperty("login").GetString(),
+                            ActivityImage  = user.GetProperty("avatar_url").GetString(),
+                            ActivitySubtitle = $"{pullRequest.GetProperty("head").GetProperty("label")} -> {pullRequest.GetProperty("base").GetProperty("label")}",
+                            Text = pullRequest.GetProperty("body").GetString(),
+                            Facts = IgnoreNulls(
+                                GetLabelFact(pullRequest.GetProperty("labels")),
+                                GetMilestoneFact(pullRequest.GetProperty("milestone"))
+                            )
+                        }
+                    },
+                    Actions = new List<OpenUriAction>
+                    {
+                        GetViewInGitHubAction(pullRequest)
+                    }
+                };
+            }
+
+            static MessageCard GetIssueCard(string eventName, JsonElement issue)
+            {
+                // issue is one of these: https://developer.github.com/v3/issues/#response
+
+                JsonElement user = issue.GetProperty("user");
+                return new MessageCard
+                {
+                    Title = $"Issue #{issue.GetProperty("number")} {eventName} - {issue.GetProperty("title").GetString()}",
+                    Summary = $"Issue #{issue.GetProperty("number")} {eventName} by { user.GetProperty("login")}, {issue.GetProperty("title")}",
+                    Sections = new List<Section>()
+                    {
+                        new Section
+                        {
+                            ActivityTitle = user.GetProperty("login").GetString(),
+                            ActivityImage  = user.GetProperty("avatar_url").GetString(),
+                            Text = issue.GetProperty("body").GetString(),
+                            Facts = IgnoreNulls(
+                                GetLabelFact(issue.GetProperty("labels")),
+                                GetMilestoneFact(issue.GetProperty("milestone"))
+                            )
+                        }
+                    },
+                    Actions = new List<OpenUriAction>
+                    {
+                        GetViewInGitHubAction(issue)
+                    }
+                };
+            }
+        }
+
+        private static OpenUriAction GetViewInGitHubAction(JsonElement element)
+        {
+            return new OpenUriAction
+            {
+                Type = ActionType.OpenUri,
+                Name = "View on GitHub",
+                Targets = new List<Target>
+                {
+                    new Target
+                    {
+                        OS = TargetOs.Default,
+                        Uri = element.GetProperty("html_url").GetString()
+                    }
+                }
+            };
+        }
+
+        private static IEnumerable<T> IgnoreNulls<T>(params T[] items)
+        {
+            var result = new List<T>(items.Where(i => i != null));
+
+            if (result.Count == 0)
+            {
+                return null;
+            }
+
+            return result;
+        }
+
+        private static Fact GetLabelFact(JsonElement labels)
+        {
+            if (labels.GetArrayLength() == 0)
+            {
+                return null;
+            }
+
+            return new Fact
+            {
+                Name = "Labels",
+                Value = string.Join(", ", labels.EnumerateArray().Select(l => l.GetProperty("name").GetString()))
+            };
+        }
+
+        private static Fact GetMilestoneFact(JsonElement milestone)
+        {
+            if (milestone.ValueKind == JsonValueKind.Null)
+            {
+                return null;
+            }
+
+            return new Fact
+            {
+                Name = "Milestone",
+                Value = milestone.GetProperty("title").GetString()
+            };
+        }
+    }
+}
